f9127a4ae3dc4b41bc7fd5134ee8c13b2736096d
Ishxan <166202329+Suraj4real@users.noreply.github.com>
2025-12-20 18:03:06 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const connection = require("./database/connection")
 const User =require("./models/userModel.js")
 const Blog =require("./models/userBlog.js")
 const app = express()
+const bcrypt = require("bcrypt")
 connection()
+//req.body ko data show gardinxa postman ma nabhaye undefined dekhauxa
+ app.use(express.json()) 
  
 
 
@@ -27,6 +30,64 @@ app.get("/fetch-users",async function(req,res){
         data :data
     })
 })
+// app.post("/register",function (req,res){
+//     console.log(req)
+// })
+app.post("/register",async function (req,res){
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    //const {name,email,password} =req.body    
+    // Alt process called  Object DeStructuring
+    console.log(name,email,password)
+    await User.create({
+        name : name,
+        email : email,
+        password :bcrypt.hashSync(password,12) //12 is a salting number
+
+    }
+    )
+    res.json({
+        message : "धन्यवाद registration ko laagi!!"
+    })
+
+    
+})
+// app.delete("/delete/:id",async function(req,res){
+//     const id = req.params.id
+//     await User.findByIdAndDelete(id)
+//     res.json({
+//         message:"Deleted successfully!!!"
+//     })
+// })
+app.delete("/delete",async function(req,res){
+    const id=req.body.id 
+    await User.findByIdAndDelete(id)
+    res.json({
+        message: "Deleted successfully!!!"
+    })
+    
+})
+app.post("/blog",async function (req,res){
+    const {title,subtitle,description} = req.body
+    console.log(title,subtitle,description)
+    await Blog.create({
+        title:title,
+        subtitle:subtitle,
+        description:description
+    })
+    res.json({
+        message:"Details haruko lagi धन्यवाद!!"
+    })
+})
+app.delete("/delete-blog/:id",async function(req,res){
+    const id = req.params.id
+    await Blog.findByIdAndDelete(id)
+    res.json({
+      message:"Deleted successfully!!!"
+    })
+})
+
 app.get("/fetch-blog",async function(req,res){
     const data = await Blog.find()
     res.json({
